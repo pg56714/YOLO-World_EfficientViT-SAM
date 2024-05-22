1cce0764a3578e323d3152015b4b756cd79aeef9
HONG,HAO-SYUAN <pg785632541@gmail.com>
2024-05-22 19:53:02 +0800

    finish

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 # YOLO-World_EfficientViT-SAM
+
+## Getting Started
+
+<!-- [HuggingFace Space]() -->
+
+![example1](/assets/example1.jpg)
+
+## Installation
+
+download the pretrained weights from the following links and save them in the `weights` directory.
+https://huggingface.co/han-cai/efficientvit-sam/resolve/main/xl1.pt
+
+Use Anaconda to create a new environment and install the required packages.
+
+```bash
+# Create and activate a python 3.10 environment.
+conda create -n yolo-world-with-efficientvit-sam python=3.10 -y
+conda activate yolo-world-with-efficientvit-sam
+pip install -r requirements.txt
+```
+
+## Running the Project
+
+```bash
+python app.py
+```
+
+## Core Models
+
+### YOLO-World
+
+[YOLO-World](https://github.com/AILab-CVC/YOLO-World) is an open-vocabulary object detection model with high efficiency.
+On the challenging LVIS dataset, YOLO-World achieves 35.4 AP with 52.0 FPS on V100,
+which outperforms many state-of-the-art methods in terms of both accuracy and speed.
+![image](https://github.com/Curt-Park/yolo-world-with-efficientvit-sam/assets/14961526/8a4a17bd-918d-478a-8451-f58e4a2dce79)
+<img width="1024" src="https://github.com/Curt-Park/yolo-world-with-efficientvit-sam/assets/14961526/fce57405-e18d-45f3-bea8-fc3971faf975">
+
+### EfficientViT SAM
+
+[EfficientViT SAM](https://github.com/mit-han-lab/efficientvit) is a new family of accelerated segment anything models.
+Thanks to the lightweight and hardware-efficient core building block,
+it delivers 48.9× measured TensorRT speedup on A100 GPU over SAM-ViT-H without sacrificing performance.
+
+<img width="1024" src="https://github.com/Curt-Park/yolo-world-with-efficientvit-sam/assets/14961526/9eec003f-47c9-43a5-86b0-82d6689e1bf9">
+<img width="1024" src="https://github.com/Curt-Park/yolo-world-with-efficientvit-sam/assets/14961526/d79973bb-0d80-4b64-a175-252de56d0d09">
+
+## Citation
+
+```
+@misc{zhang2024efficientvitsam,
+  title={EfficientViT-SAM: Accelerated Segment Anything Model Without Performance Loss},
+  author={Zhuoyang Zhang and Han Cai and Song Han},
+  year={2024},
+  eprint={2402.05008},
+  archivePrefix={arXiv},
+  primaryClass={cs.CV}
+}
+
+@article{cheng2024yolow,
+  title={YOLO-World: Real-Time Open-Vocabulary Object Detection},
+  author={Cheng, Tianheng and Song, Lin and Ge, Yixiao and Liu, Wenyu and Wang, Xinggang and Shan, Ying},
+  journal={arXiv preprint arXiv:2401.17270},
+  year={2024}
+}
+```
---
+++ app.py
@@ -0,0 +1,135 @@
+import os
+import cv2
+import gradio as gr
+import numpy as np
+import supervision as sv
+import torch
+from inference.models import YOLOWorld
+
+from efficientvit.models.efficientvit.sam import EfficientViTSamPredictor
+from efficientvit.sam_model_zoo import create_sam_model
+
+
+MARKDOWN = """
+# YOLO-World + EfficientViT-SAM
+Powered by Roboflow [Inference](https://github.com/roboflow/inference) and [Supervision](https://github.com/roboflow/supervision) and [YOLO-World](https://github.com/AILab-CVC/YOLO-World) and [EfficientViT-SAM](https://github.com/mit-han-lab/efficientvit)
+"""
+
+# Load models
+yolo_world = YOLOWorld(model_id="yolo_world/l")
+device = "cuda" if torch.cuda.is_available() else "cpu"
+sam = EfficientViTSamPredictor(
+    create_sam_model(name="xl1", weight_url="./weights/xl1.pt").to(device).eval()
+)
+
+
+# Load annotators
+BOUNDING_BOX_ANNOTATOR = sv.BoundingBoxAnnotator()
+MASK_ANNOTATOR = sv.MaskAnnotator()
+LABEL_ANNOTATOR = sv.LabelAnnotator()
+
+
+def detect(
+    image: np.ndarray,
+    query: str,
+    confidence_threshold: float,
+    nms_threshold: float,
+) -> np.ndarray:
+    # Preparation.
+    categories = [category.strip() for category in query.split(",")]
+    yolo_world.set_classes(categories)
+    # print("categories:", categories)
+
+    # Object detection
+    results = yolo_world.infer(image, confidence=confidence_threshold)
+    detections = sv.Detections.from_inference(results).with_nms(
+        class_agnostic=True, threshold=nms_threshold
+    )
+    # print("detected:", detections)
+
+    # Segmentation
+    sam.set_image(image, image_format="RGB")
+    masks = []
+    for xyxy in detections.xyxy:
+        mask, _, _ = sam.predict(box=xyxy, multimask_output=False)
+        masks.append(mask.squeeze())
+    detections.mask = np.array(masks)
+    # print("masks shaped as", detections.mask.shape)
+
+    # Annotation
+    output_image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    labels = [
+        f"{categories[class_id]}: {confidence:.2f}"
+        for class_id, confidence in zip(detections.class_id, detections.confidence)
+    ]
+    output_image = MASK_ANNOTATOR.annotate(output_image, detections)
+    output_image = BOUNDING_BOX_ANNOTATOR.annotate(output_image, detections)
+    output_image = LABEL_ANNOTATOR.annotate(output_image, detections, labels=labels)
+    return cv2.cvtColor(output_image, cv2.COLOR_BGR2RGB)
+
+
+with gr.Blocks() as demo:
+    gr.Markdown(MARKDOWN)
+    with gr.Row():
+        with gr.Column():
+            input_image_component = gr.Image(type="numpy", label="Input Image")
+            image_categories_text_component = gr.Textbox(
+                placeholder="you can input multiple words with comma (,)",
+            )
+
+            with gr.Accordion("YOLO-WORLD", open=False):
+                confidence_threshold_component = gr.Slider(
+                    minimum=0,
+                    maximum=1,
+                    value=0.3,
+                    step=0.01,
+                    label="Confidence Threshold",
+                )
+
+                iou_threshold_component = gr.Slider(
+                    minimum=0,
+                    maximum=1,
+                    value=0.5,
+                    step=0.01,
+                    label="NMS Threshold",
+                )
+
+        yolo_world_output_image_component = gr.Image(type="numpy", label="Output image")
+    submit_button_component = gr.Button(value="Submit", scale=1, variant="primary")
+    gr.Examples(
+        fn=detect,
+        examples=[
+            [
+                os.path.join(os.path.dirname(__file__), "examples/livingroom.jpg"),
+                "table, lamp, dog, sofa, plant, clock, carpet, frame on the wall",
+                0.05,
+                0.5,
+            ],
+            [
+                os.path.join(os.path.dirname(__file__), "examples/cat_and_dogs.jpg"),
+                "cat, dog",
+                0.2,
+                0.5,
+            ],
+        ],
+        inputs=[
+            input_image_component,
+            image_categories_text_component,
+            confidence_threshold_component,
+            iou_threshold_component,
+        ],
+        outputs=yolo_world_output_image_component,
+    )
+
+    submit_button_component.click(
+        fn=detect,
+        inputs=[
+            input_image_component,
+            image_categories_text_component,
+            confidence_threshold_component,
+            iou_threshold_component,
+        ],
+        outputs=yolo_world_output_image_component,
+    )
+
+demo.launch(debug=False, show_error=True)
